18728380318becc0aa8b0695889d1fa09916da2b
Farid T <31551629+artificialfintelligence@users.noreply.github.com>
2022-05-22 00:00:46 +0900

    Kept the naive solver (no graphs/edges) but added all features.

--- myletterboxedsolver.py
+++ myletterboxedsolver.py
@@ -9,8 +9,9 @@ This essentially borrows all of its functionality from three existing projects a
 """
 
 
-from ast import Num
+from utils import puzzle_fetcher
 import sys
+import re
 
 
 # The Trie data structure which'll hold our dictionary of valid words.
@@ -143,25 +144,103 @@ class Puzzle(object):
             return solution
 
 
-def main(*args, **kwargs):
-    pass
+def main(**kwargs):
+    if '--len' in kwargs:
+        try:
+            max_soln_length = int(kwargs['--len'])
+        except:
+            print(f'Invalid maximum solution length: {kwargs["--len"]}')
+            return
+    else:
+        max_soln_length = 2
+    if max_soln_length > 9:
+        print(f'Error: Maximum solution length must be less than 10.')
+        return
+
+    if '--dict' in kwargs:
+        dict_file_path = kwargs['--dict']
+    else:
+        dict_file_path = 'resources/dict_S.txt'
+    try:
+        f = open(dict_file_path, 'r')
+        words = f.read().strip().split('\n')
+        words = [word.lower() for word in words if len(word)>2]
+    except Exception as e:
+        print(f'Failed to open dictionary file {dict_file_path}:\n {e}')
+        return
+    f.close()
+    dictionary = Trie(words)
+
+    if '--puzzle' in kwargs:
+        puzzle_string = kwargs['--puzzle']
+        if not bool(re.match('\A([a-z]+-)+[a-z]+$', puzzle_string)):
+            print(f'Invalid puzzle string: {puzzle_string}')
+            return
+    else:
+        try:
+            puzzle_string = puzzle_fetcher.fetch()
+        except:
+            print(f'Failed to fetch today\'s puzzle from the NYT website.')
+            return
+    puzzle = Puzzle(dictionary, puzzle_string)
+
+    print(f"\nNow solving {puzzle_string}...")
+    all_solutions = puzzle.find_solutions(max_soln_length)
+    print(f"\nFound {len(all_solutions)} solutions of length {max_soln_length} or less.")
+
+    if '--output' in kwargs:
+        output_file_path = kwargs['--output']
+        try:
+            f = open(output_file_path, 'w') 
+        except Exception as e:
+            print(f'Failed to open output file {output_file_path}:\n {e}')
+            return
+        print(f'\nWriting results to {output_file_path}...')
+        for solution in all_solutions:
+            f.write(f'{solution}\n')
+        f.close()
 
 if __name__ == '__main__':
-    # # Parse command line arguments and invoke main() accordingly
-    # main(sys.argv[1], # foo
-    #         sys.argv[2], # bar
-    #         **dict(arg.split('=') for arg in sys.argv[3:])) # kwargs
+    # Parse command line arguments and invoke main() accordingly
+    usage_guide = f'''
+            Usage: myletterboxedsolver [--option <value>] 
+            
+            Where:
+            
+            --puzzle <abc-def-ghi-jkl>
+            Solves the provided puzzle (must contain at least one
+            hyphen). If ommitted, will attempt to retrieve today's
+            puzzle from the NYT website.
+            
+            --len <M>
+            Limits solutions to those with a maximum of M guesses.
+            (Default = 2)
+            
+            --dict <path_to_dictionary_file>
+            Uses the provided dictionary of valid words. (Note: Words
+            shorter than 3 letters will be ignored). 
+            (default = resources/dict_S.txt)
+
+            --output <path_to_output_file>
+            Writes solutions to the specified file. WARNING: If the file
+            already exists, it will be over-written.
+            (default: Do not output results)
+
+            Example:
+            myletterboxedsolver --puzzle abc-def-ghi --len 3 --dict words.txt
+            '''
+    args = sys.argv[1:]
+    valid_options = ['puzzle', 'len', 'dict', 'output']
+    valid_keys = list(map(lambda x: '--' + x, valid_options))
+    values = args[1::2]
+    keys = args[::2]
+    if len(args) % 2 != 0:
+        print(usage_guide)
+    else:
+        for key in keys:
+            if key not in valid_keys:
+                print(usage_guide)
+                break
+        else:
+            main(**dict(zip(keys, values)))
     pass
-
-# First, we create our dictionary of all valid words.
-f = open('dict_M.txt', 'r')
-words = f.read().strip().split('\n')
-words = [word.lower() for word in words if len(word)>2]
-f.close()
-
-dictionary = Trie(words)
-puzzle = Puzzle(dictionary, 'mrf-sna-opu-gci')
-max_soln_length = 2
-
-all_solutions = puzzle.find_solutions(max_soln_length)
-print(f"Found {len(all_solutions)} solutions of length {max_soln_length} or less.")
