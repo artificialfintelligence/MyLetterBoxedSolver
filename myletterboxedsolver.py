8bcb02dd7a4cc781849306466b9ac3e0e8d1ac4c
Farid T <31551629+artificialfintelligence@users.noreply.github.com>
2022-06-16 18:23:05 +0900

    added requirements; auto-formatted source w/ black

--- myletterboxedsolver.py
+++ myletterboxedsolver.py
@@ -16,7 +16,6 @@ import re
 
 # The Trie data structure which'll hold our dictionary of valid words.
 class Trie(object):
-    
     def __init__(self, words: list[str] = None) -> None:
         self.trie = dict()
         if words is not None:
@@ -33,7 +32,7 @@ class Trie(object):
         # the root, by adding the 'valid_word' key. Note: In this
         # implementation, only the existence of this special key
         # matters. not the associated value.
-        current_node['valid_word'] = True   # Or whatever!
+        current_node["valid_word"] = True  # Or whatever!
 
     """
     Search the trie for a specified word.
@@ -43,6 +42,7 @@ class Trie(object):
     * 0 if word is a prefix of some words in our wordlist
     * 1 if word is a word in our wordlist (and possibly a prefix to a longer word!)
     """
+
     def query(self, word: str) -> int:
         current_node = self.trie
         for letter in word:
@@ -50,7 +50,7 @@ class Trie(object):
                 return -1
             current_node = current_node[letter]
         else:
-            if 'valid_word' in current_node:
+            if "valid_word" in current_node:
                 return 1
             else:
                 return 0
@@ -58,31 +58,26 @@ class Trie(object):
 
 # The Puzzle class which'll hold puzzle-specific data and methods.
 class Puzzle(object):
-
-    def __init__(
-            self, 
-            dictionary: Trie, 
-            sides: str, 
-            delim: str = '-') -> None:
+    def __init__(self, dictionary: Trie, sides: str, delim: str = "-") -> None:
         self.dictionary = dictionary
         self.sides = sides.lower().split(delim)
         self.all_letters = list(
-            letter for side in self.sides for letter in side)
+            letter for side in self.sides for letter in side
+        )
         self.num_letters = len(self.all_letters)
         self.all_valid_words = self.find_all_words()
         self.words_by_1st_letter = self.map_valid_words()
-    
+
     def find_all_words(
-            self,
-            starting_with:str = None,
-            current_side_idx: int = 0) -> list[str]:
+        self, starting_with: str = None, current_side_idx: int = 0
+    ) -> list[str]:
         if starting_with == None:
             valid_words = []
             for first_side_idx in range(len(self.sides)):
                 for first_letter in self.sides[first_side_idx]:
                     valid_words += self.find_all_words(
-                            first_letter,
-                            first_side_idx)
+                        first_letter, first_side_idx
+                    )
             return valid_words
         else:
             valid_words = []
@@ -96,31 +91,29 @@ class Puzzle(object):
                         if query_result == 1:
                             valid_words.append(candidate)
                         valid_words += self.find_all_words(
-                                candidate, 
-                                next_side_idx)
+                            candidate, next_side_idx
+                        )
             return valid_words
 
     def map_valid_words(self) -> dict:
         valid_words_map = {}
         for l in self.all_letters:
-            valid_words_map[l] = list(filter(
-                    lambda x: x[0] == l, 
-                    self.all_valid_words.copy()))
+            valid_words_map[l] = list(
+                filter(lambda x: x[0] == l, self.all_valid_words.copy())
+            )
         return valid_words_map
 
     def find_solutions(
-            self,
-            max_length: int = 10,
-            candidate:list[str] = None,
-            used_letters:set[str] = None) -> list[list[str]]:
+        self,
+        max_length: int = 10,
+        candidate: list[str] = None,
+        used_letters: set[str] = None,
+    ) -> list[list[str]]:
         # Initial word (head of recursion stack):
         if candidate == None:
             solutions = []
             for w in self.all_valid_words:
-                solutions += self.find_solutions(
-                        max_length,
-                        [w],
-                        set(w))
+                solutions += self.find_solutions(max_length, [w], set(w))
             return solutions
         # Recursive call:
         else:
@@ -130,7 +123,7 @@ class Puzzle(object):
                 return [candidate]
             elif len(candidate) == max_length:
                 return []
-            
+
             # last letter of the last word
             next_letter = candidate[-1][-1]
             solution = []
@@ -139,78 +132,89 @@ class Puzzle(object):
                 if new_letters:
                     solution += self.find_solutions(
                         max_length,
-                        candidate + [next_word], 
-                        used_letters | new_letters)
+                        candidate + [next_word],
+                        used_letters | new_letters,
+                    )
             return solution
 
 
 def main(**kwargs):
-    if '--len' in kwargs:
+    if "--len" in kwargs:
         try:
-            max_soln_length = int(kwargs['--len'])
+            max_soln_length = int(kwargs["--len"])
         except:
             print(f'Invalid maximum solution length: {kwargs["--len"]}')
             return
     else:
         max_soln_length = 2
     if max_soln_length > 9:
-        print(f'Error: Maximum solution length must be less than 10.')
+        print(f"Error: Maximum solution length must be less than 10.")
         return
 
-    if '--dict' in kwargs:
-        dict_file_path = kwargs['--dict']
+    if "--dict" in kwargs:
+        dict_file_path = kwargs["--dict"]
     else:
-        dict_file_path = 'resources/dict_S.txt'
+        dict_file_path = "resources/dict_S.txt"
     try:
-        f = open(dict_file_path, 'r')
-        words = f.read().strip().split('\n')
-        words = [word.lower() for word in words if len(word)>2]
+        f = open(dict_file_path, "r")
+        words = f.read().strip().split("\n")
+        words = [word.lower() for word in words if len(word) > 2]
     except Exception as e:
-        print(f'Failed to open dictionary file {dict_file_path}:\n {e}')
+        print(f"Failed to open dictionary file {dict_file_path}:\n {e}")
         return
     f.close()
     dictionary = Trie(words)
 
-    if '--puzzle' in kwargs:
-        puzzle_string = kwargs['--puzzle']
-        if not bool(re.match('\A([a-z]+-)+[a-z]+$', puzzle_string)):
-            print(f'Invalid puzzle string: {puzzle_string}')
+    if "--puzzle" in kwargs:
+        puzzle_string = kwargs["--puzzle"]
+        if not bool(re.match("\A([a-z]+-)+[a-z]+$", puzzle_string)):
+            print(f"Invalid puzzle string: {puzzle_string}")
             return
     else:
         try:
             puzzle_string = puzzle_fetcher.fetch()
         except:
-            print(f'Failed to fetch today\'s puzzle from the NYT website.')
+            print(f"Failed to fetch today's puzzle from the NYT website.")
             return
     puzzle = Puzzle(dictionary, puzzle_string)
 
     print(f"\nFound {len(puzzle.all_valid_words)} valid words.")
     print(f"\nSolving {puzzle_string}...")
     all_solutions = puzzle.find_solutions(max_soln_length)
-    print(f"\nFound {len(all_solutions)} solutions of length {max_soln_length} or less.")
+    print(
+        f"\nFound {len(all_solutions)} solutions of length {max_soln_length} or less."
+    )
 
-    if '--output' in kwargs:
-        output_file_path = kwargs['--output']
+    if "--output" in kwargs:
+        output_file_path = kwargs["--output"]
         try:
-            f = open(output_file_path, 'w') 
+            f = open(output_file_path, "w")
         except Exception as e:
-            print(f'Failed to open output file {output_file_path}:\n {e}')
+            print(f"Failed to open output file {output_file_path}:\n {e}")
             return
-        print(f'\nWriting results to {output_file_path}...')
+        print(f"\nWriting results to {output_file_path}...")
         for solution in all_solutions:
-            f.write(f'{solution}\n')
+            f.write(f"{solution}\n")
         f.close()
     elif len(all_solutions) != 0:
-        show_output = input('\nOutput file not specified. Output all solutions to the terminal? (y/N): ') or 'n'
-        while show_output.lower() not in {'y', 'n'}:
-            show_output = input('Output all solutions to the terminal? (y/N): ') or 'n'
-        if show_output.lower() == 'y':
+        show_output = (
+            input(
+                "\nOutput file not specified. Output all solutions to the terminal? (y/N): "
+            )
+            or "n"
+        )
+        while show_output.lower() not in {"y", "n"}:
+            show_output = (
+                input("Output all solutions to the terminal? (y/N): ") or "n"
+            )
+        if show_output.lower() == "y":
             for solution in all_solutions:
-                    print(f'{solution}')
+                print(f"{solution}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Parse command line arguments and invoke main() accordingly
-    usage_guide = f'''
+    usage_guide = f"""
             Usage: myletterboxedsolver [--option <value>] 
             
             Where:
@@ -236,10 +240,10 @@ if __name__ == '__main__':
 
             Example:
             myletterboxedsolver --puzzle abc-def-ghi --len 3 --dict words.txt
-            '''
+            """
     args = sys.argv[1:]
-    valid_options = ['puzzle', 'len', 'dict', 'output']
-    valid_keys = list(map(lambda x: '--' + x, valid_options))
+    valid_options = ["puzzle", "len", "dict", "output"]
+    valid_keys = list(map(lambda x: "--" + x, valid_options))
     values = args[1::2]
     keys = args[::2]
     if len(args) % 2 != 0:
--- utils/puzzle_fetcher.py
+++ utils/puzzle_fetcher.py
@@ -1,20 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def fetch() -> str:
     """
     Return today's puzzle grouped by side with groups separated by hyphens
     """
-    response = requests.get('https://www.nytimes.com/puzzles/letter-boxed')
-    soup = BeautifulSoup(response.text, 'html.parser')
-    scripts = soup.find_all('script')
-    game_data_script = [script.text for script in scripts
-                        if script.text.startswith('window.gameData')][0]
+    response = requests.get("https://www.nytimes.com/puzzles/letter-boxed")
+    soup = BeautifulSoup(response.text, "html.parser")
+    scripts = soup.find_all("script")
+    game_data_script = [
+        script.text
+        for script in scripts
+        if script.text.startswith("window.gameData")
+    ][0]
     search = '"sides":'
     sides_start = game_data_script.index(search) + len(search) + 1
-    length = len('"ABC"') * 4 + len(',') * 3
-    sides_data = game_data_script[sides_start:sides_start + length]   # "UPO","XTS","EIL","NCY"
-    return sides_data.replace('"', '').replace(',', '-')
+    length = len('"ABC"') * 4 + len(",") * 3
+    sides_data = game_data_script[
+        sides_start : sides_start + length
+    ]  # "UPO","XTS","EIL","NCY"
+    return sides_data.replace('"', "").replace(",", "-")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(fetch())
